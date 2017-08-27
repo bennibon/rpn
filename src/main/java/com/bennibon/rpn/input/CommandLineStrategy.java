b9e588f51c644da33504adbc191b4b6a0a1b4b89
Ben Bonavia <bennibon@gmail.com>
2017-08-27 18:44:29 +1000

    Switched from maven build to gradle build

--- src/main/java/com/bennibon/rpn/input/CommandLineStrategy.java
+++ src/main/java/com/bennibon/rpn/input/CommandLineStrategy.java
@@ -2,48 +2,46 @@ package com.bennibon.rpn.input;
 
 import java.util.Scanner;
 
-import com.bennibon.rpn.calc.types.Operator;
-
 /**
- * The command line input strategy. 
+ * The command line input strategy.
  * <p>
  * This strategy takes the input from the command line.
  * @author Ben Bonavia 2017
  */
 public class CommandLineStrategy extends InputStrategy {
-	
-	/** string to quit. */
-	private static final String QUIT = "quit";
-	
-	/** The opening banner with quit instruction. */
-	private static final String CLI_BANNER = BANNER + "\nType \"quit\" to quit\n";
-	
-	@Override
-	public void execute() {
-		System.out.println(CLI_BANNER);
-		Scanner inputScanner = new Scanner(System.in);
-		awaitInput(inputScanner);
-		
-		// close scanner when finished
-		inputScanner.close(); 
-	}
-	
-	/**
-	 * Await input from a scanner.
-	 * @param inputScanner The input scanner
-	 */
-	final private void awaitInput(Scanner inputScanner) {
-		
-		while (true) {
-			System.out.print(PROMPT);
-			if (inputScanner.hasNextLine()) {
-				String inputLine = inputScanner.nextLine();
-				if (inputLine.toLowerCase().contains(QUIT)) {
-					break;
-				}
-				calc.process(inputLine);
-			}
-		}
-	}
+
+    /** string to quit. */
+    private static final String QUIT = "quit";
+
+    /** The opening banner with quit instruction. */
+    private static final String CLI_BANNER = BANNER + "\nType \"quit\" to quit\n";
+
+    @Override
+    public void execute() {
+        System.out.println(CLI_BANNER);
+        System.out.print(PROMPT);
+        final Scanner inputScanner = new Scanner(System.in);
+        awaitInput(inputScanner);
+
+        // close scanner when finished
+        inputScanner.close();
+    }
+
+    /**
+     * Await input from a scanner.
+     * @param inputScanner The input scanner
+     */
+    final private void awaitInput(final Scanner inputScanner) {
+        while (true) {
+            if (inputScanner.hasNextLine()) {
+                final String inputLine = inputScanner.nextLine();
+                if (inputLine.toLowerCase().contains(QUIT)) {
+                    break;
+                }
+                calc.process(inputLine);
+                System.out.print(PROMPT);
+            }
+        }
+    }
 
 }
